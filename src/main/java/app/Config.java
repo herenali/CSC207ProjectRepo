a1add1b045ffd46ead2d76622c2d9811d1e1f6b6
herenali <herenalihoiching@gmail.com>
2024-11-04 16:51:05 -0500

    Update config file

--- src/main/java/app/Config.java
+++ src/main/java/app/Config.java
@@ -2,8 +2,8 @@ package app;
 
 public class Config {
     // set your api key here
-    public static String apiToken = "";
+    public static String apiToken = "e4fbd0788231cf40830bf62f866aa001182f9971";
     // set app id here
-    public static String appId = "";
+    public static String appId = "049E2510-3508-4C99-80F9-A3C24ECA7677";
 
 }
