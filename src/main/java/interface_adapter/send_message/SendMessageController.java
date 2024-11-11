ecb93e25a14d570af0e8f35d74887b84b177202e
jayceelaw <jayceelaw@gmail.com>
2024-11-11 16:22:24 -0500

    added interface adapter for send_message (incomplete)

---
+++ src/main/java/interface_adapter/send_message/SendMessageController.java
@@ -0,0 +1,29 @@
+package interface_adapter.send_message;
+
+import use_case.send_message.SendMessageInputBoundary;
+import use_case.send_message.SendMessageInputData;
+
+/**
+ * The controller for the Send Message Use Case.
+ */
+public class SendMessageController {
+    private final SendMessageInputBoundary sendMessageCaseInteractor;
+
+    public SendMessageController(SendMessageInputBoundary sendMessageCaseInteractor) {
+        this.sendMessageCaseInteractor = sendMessageCaseInteractor;
+    }
+
+    /**
+     * Executes the Send Message Use Case.
+     * @param userId the user ID of the user logging in
+     * @param message the message contents to send
+     */
+    public void execute(String userId, String message) {
+        // 1. instantiate the `SendMessageData`, which should contain the username.
+        // 2. tell the Interactor to execute.
+        final SendMessageInputData sendMessageInputData = new SendMessageInputData(userId, message);
+
+        sendMessageCaseInteractor.execute(sendMessageInputData);
+    }
+
+}
