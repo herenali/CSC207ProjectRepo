f4b0d4e54f74423881db9c5a176381dca715d367
LavenderLO <lolavender528@gmail.com>
2024-11-11 16:25:49 -0500

    Modify SendMessageOutputData

--- src/main/java/use_case/send_message/SendMessageOutputData.java
+++ src/main/java/use_case/send_message/SendMessageOutputData.java
@@ -4,5 +4,25 @@ package use_case.send_message;
  * Output Data for the Send Message Use Case.
  */
 public class SendMessageOutputData {
+    private final String userId;
+    private final String message;
+    private final boolean useCaseFailed;
 
+    public SendMessageOutputData(String userId, String message, boolean useCaseFailed) {
+        this.userId = userId;
+        this.message = message;
+        this.useCaseFailed = useCaseFailed;
+    }
+
+    String getUserId() {
+        return userId;
+    }
+
+    String getMessage() {
+        return message;
+    }
+
+    public boolean isUseCaseFailed() {
+        return useCaseFailed;
+    }
 }
